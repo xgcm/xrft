3b342225dd645c6882e80a52981c6e8c52848143
Ryan Abernathey <rpa@ldeo.columbia.edu>
2016-06-10 17:24:00 -0400

    starting to put code in

--- xrft/xrft.py
+++ xrft/xrft.py
@@ -1,210 +1,85 @@
 import numpy as np
-import pandas as pd
-import scipy.optimize as opt
-from scipy.special import erf
+import xarray as xr
 from .due import due, Doi
 
-__all__ = ["Model", "Fit", "opt_err_func", "transform_data", "cumgauss"]
+__all__ = ["dft"]
 
 
-# Use duecredit (duecredit.org) to provide a citation to relevant work to
-# be cited. This does nothing, unless the user has duecredit installed,
-# And calls this with duecredit (as in `python -m duecredit script.py`):
-#due.cite(Doi("10.1167/13.9.30"),
-#         description="Template project for small scientific Python projects",
-#         tags=["reference-implementation"],
-#         path='xrft')
-
-
-def transform_data(data):
+def dft(da, dim=None, shift=True, remove_mean=True, density=False):
     """
-    Function that takes experimental data and gives us the
-    dependent/independent variables for analysis
+    Perform discrete Fourier transform of xarray data-array `da` along the
+    specified dimensions.
 
     Parameters
     ----------
-    data : Pandas DataFrame or string.
-        If this is a DataFrame, it should have the columns `contrast1` and
-        `answer` from which the dependent and independent variables will be
-        extracted. If this is a string, it should be the full path to a csv
-        file that contains data that can be read into a DataFrame with this
-        specification.
+    da : `xarray.DataArray`
+        The data to be transformed
+    dim : list (optional)
+        The dimensions along which to take the transformation. If `None`, all
+        dimensions will be transformed.
+    shift : bool (optional)
+        Whether to shift the fft output.
+    remove_mean : bool (optional)
+        If `True`, the mean across the transform dimensions will be subtracted
+        before calculating the Fourier transform.
+    density : bool (optional)
+        If `True`, the output will be normalized to give spectral density.
 
     Returns
     -------
-    x : array
-        The unique contrast differences.
-    y : array
-        The proportion of '2' answers in each contrast difference
-    n : array
-        The number of trials in each x,y condition
+    daft : `xarray.DataArray`
+        The output of the Fourier transformation, with appropriate dimensions.
     """
-    if isinstance(data, str):
-        data = pd.read_csv(data)
 
-    contrast1 = data['contrast1']
-    answers = data['answer']
+    if dim is None:
+        dim = da.dims
 
-    x = np.unique(contrast1)
-    y = []
-    n = []
+    # the axes along which to take ffts
+    axis_num = [da.get_axis_num(d) for d in dim]
 
-    for c in x:
-        idx = np.where(contrast1 == c)
-        n.append(float(len(idx[0])))
-        answer1 = len(np.where(answers[idx[0]] == 1)[0])
-        y.append(answer1 / n[-1])
-    return x, y, n
+    N = [da.shape[n] for n in axis_num]
 
+    # verify even spacing of input coordinates
+    delta_x = []
+    for d in dim:
+        diff = np.diff(da[d])
+        if not np.allclose(diff, diff[0]):
+            raise ValueError("Can't take Fourier transform because"
+                             "coodinate %s is not evenly spaced" % d)
+        delta_x.append(diff[0])
+    # calculate frequencies from coordinates
+    k = [ np.fft.fftfreq(Nx, dx) for (Nx, dx) in zip(N, delta_x)]
 
-def cumgauss(x, mu, sigma):
-    """
-    The cumulative Gaussian at x, for the distribution with mean mu and
-    standard deviation sigma.
+    if remove_mean:
+        da = da - da.mean(dim=dim)
 
-    Parameters
-    ----------
-    x : float or array
-       The values of x over which to evaluate the cumulative Gaussian function
+    # the hard work
+    f = np.fft.fftn(da.values, axes=axis_num)
 
-    mu : float
-       The mean parameter. Determines the x value at which the y value is 0.5
+    if shift:
+        f = np.fft.fftshift(f, axes=axis_num)
+        k = [np.fft.fftshift(l) for l in k]
 
-    sigma : float
-       The variance parameter. Determines the slope of the curve at the point
-       of Deflection
+    dk = [l[1] - l[0] for l in k]
 
-    Returns
-    -------
+    if density:
+        spectral_volume = reduce(lambda x, y: x*y, dk)
+        f /= spectral_volume
 
-    g : float or array
-        The cumulative gaussian with mean $\\mu$ and variance $\\sigma$
-        evaluated at all points in `x`.
+    # set up new dimensions for dataarray
+    k_names = ['freq_'+d for d in dim]
+    k_coords = { key: val for (key,val) in zip(k_names, k)}
 
-    Notes
-    -----
-    Based on:
-    http://en.wikipedia.org/wiki/Normal_distribution#Cumulative_distribution_function
+    newdims = list(da.dims)
+    for anum, d in zip(axis_num, dim):
+        newdims[anum] = 'freq_' + d
 
-    The cumulative Gaussian function is defined as:
+    newcoords = {}
+    for d in newdims:
+        if d in da.coords:
+            newcoords[d] = da.coords[d]
+        else:
+            newcoords[d] = k_coords[d]
 
-    .. math::
-
-        \\Phi(x) = \\frac{1}{2} [1 + erf(\\frac{x}{\\sqrt{2}})]
-
-    Where, $erf$, the error function is defined as:
-
-    .. math::
-
-        erf(x) = \\frac{1}{\\sqrt{\\pi}} \int_{-x}^{x} e^{t^2} dt
-
-    """
-    return 0.5 * (1 + erf((x - mu) / (np.sqrt(2) * sigma)))
-
-
-def opt_err_func(params, x, y, func):
-    """
-    Error function for fitting a function using non-linear optimization
-
-    Parameters
-    ----------
-    params : tuple
-        A tuple with the parameters of `func` according to their order of
-        input
-
-    x : float array
-        An independent variable.
-
-    y : float array
-        The dependent variable.
-
-    func : function
-        A function with inputs: `(x, *params)`
-
-    Returns
-    -------
-    float array
-        The marginals of the fit to x/y given the params
-    """
-    return y - func(x, *params)
-
-
-class Model(object):
-    """ Class for fitting cumulative Gaussian functions to data"""
-    def __init__(self, func=cumgauss):
-        """ Initialize a model object
-
-        Parameters
-        ----------
-        data : Pandas DataFrame
-            Data from a subjective contrast judgement experiment
-
-        func : callable, optional
-            A function that relates x and y through a set of parameters.
-            Default: :func:`cumgauss`
-        """
-        self.func = func
-
-    def fit(self, x, y, initial=[0.5, 1]):
-        """
-        Fit a Model to data
-
-        Parameters
-        ----------
-        x : float or array
-           The independent variable: contrast values presented in the
-           experiment
-        y : float or array
-           The dependent variable
-
-        Returns
-        -------
-        fit : :class:`Fit` instance
-            A :class:`Fit` object that contains the parameters of the model.
-
-        """
-        params, _ = opt.leastsq(opt_err_func, initial,
-                                args=(x, y, self.func))
-        return Fit(self, params)
-
-
-class Fit(object):
-    """
-    Class for representing a fit of a model to data
-    """
-    def __init__(self, model, params):
-        """
-        Initialize a :class:`Fit` object
-
-        Parameters
-        ----------
-        model : a :class:`Model` instance
-            An object representing the model used
-
-        params : array or list
-            The parameters of the model evaluated for the data
-
-        """
-        self.model = model
-        self.params = params
-
-    def predict(self, x):
-        """
-        Predict values of the dependent variable based on values of the
-        indpendent variable.
-
-        Parameters
-        ----------
-        x : float or array
-            Values of the independent variable. Can be values presented in
-            the experiment. For out-of-sample prediction (e.g. in
-            cross-validation), these can be values
-            that were not presented in the experiment.
-
-        Returns
-        -------
-        y : float or array
-            Predicted values of the dependent variable, corresponding to
-            values of the independent variable.
-        """
-        return self.model.func(x, *self.params)
+    for this_dk, d in zip(dk, dim):
+        newcoords['freq_' + d + '_spacing'] = this_dk
