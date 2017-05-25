60cccd06af63de69f7c792ab70244c3abda70918
Takaya Uchida <takaya@ldeo.columbia.edu>
2017-05-25 13:21:38 -0400

    Isotropic (#4)
    * added Python 3 compatibility for the reduce function
    
    * added isotropic PSD calculation and example notebook
    
    * added test for spectral slope on synthetic data
    
    * Divided up functions and the test is passing on local environment
    
    * Improved documentation of each function and implemented the azimuthal averaging as an implicit function
    
    * Fixed the cross spectrum function
    
    * Cross spectrum function now runs
    
    * Added Hanning window function. I'm sure the _hanning function can be written cleaner but I'm not quite sure how...
    
    * Added unit test
    
    * Forgot to push the actual xrft.py code
    
    * editted unit test
    
    * added a test to see there are no nans in power spectra
    
    * added a test to see there are no masked values in power spectra
    
    * added a test to see whether the density calculation is gicing the error
    
    * changed the Python requirement to Python 3.6
    
    * The hanning function didnt need any exception arguments because it takes the dimensions of da given

--- xrft/tests/test_xrft.py
+++ xrft/tests/test_xrft.py
@@ -45,6 +45,11 @@ def test_dft_1d():
     npt.assert_allclose(ft_data_expected, ft.values)
 
     # modify data to be non-evenly spaced
+    da2 = da.copy()
+    da2[-1] = np.nan
+    with pytest.raises(ValueError):
+        ft = xrft.dft(da2)
+
     da['x'].values[-1] *= 2
     with pytest.raises(ValueError):
         ft = xrft.dft(da)
@@ -61,3 +66,235 @@ def test_dft_1d_time():
     dt = (time[1] - time[0]).total_seconds()
     freq_time_expected = np.fft.fftshift(np.fft.fftfreq(Nt, dt))
     npt.assert_allclose(ft['freq_time'], freq_time_expected)
+
+def test_dft_2d():
+    """Test the discrete Fourier transform on 2D data"""
+    N = 16
+    da = xr.DataArray(np.random.rand(N,N), dims=['x','y'],
+                    coords={'x':range(N),'y':range(N)}
+                     )
+    ft = xrft.dft(da, shift=False, remove_mean=False)
+    npt.assert_almost_equal(ft.values, np.fft.fftn(da.values))
+
+    ft = xrft.dft(da, shift=False, window=True)
+    dim = da.dims
+    window = np.hanning(N) * np.hanning(N)[:, np.newaxis]
+    da_prime = (da - da.mean(dim=dim)).values
+    npt.assert_almost_equal(ft.values, np.fft.fftn(da_prime*window))
+
+def test_power_spectrum():
+    """Test the power spectrum function"""
+    N = 16
+    da = xr.DataArray(np.random.rand(N,N), dims=['x','y'],
+                    coords={'x':range(N),'y':range(N)}
+                     )
+    ps = xrft.power_spectrum(da, window=True, density=False)
+    daft = xrft.dft(da,
+                    dim=None, shift=True, remove_mean=True,
+                    window=True)
+    npt.assert_almost_equal(ps.values, np.real(daft*np.conj(daft)))
+    npt.assert_almost_equal(np.ma.masked_invalid(ps).mask.sum(), 0.)
+
+    ### Normalized
+    dim = da.dims
+    ps = xrft.power_spectrum(da, window=True)
+    coord = list(daft.coords)
+    daft = xrft.dft(da,
+                    dim=None, shift=True, remove_mean=True,
+                    window=True)
+    test = np.real(daft*np.conj(daft))/N**4
+    for i in range(len(dim)):
+        test /= daft[coord[-i-1]].values
+    npt.assert_almost_equal(ps.values, test)
+    npt.assert_almost_equal(np.ma.masked_invalid(ps).mask.sum(), 0.)
+
+def test_cross_spectrum():
+    """Test the cross spectrum function"""
+    N = 16
+    da = xr.DataArray(np.random.rand(N,N), dims=['x','y'],
+                    coords={'x':range(N),'y':range(N)}
+                     )
+    da2 = xr.DataArray(np.random.rand(N,N), dims=['x','y'],
+                    coords={'x':range(N),'y':range(N)}
+                     )
+    cs = xrft.cross_spectrum(da, da2, window=True, density=False)
+    daft = xrft.dft(da,
+                    dim=None, shift=True, remove_mean=True,
+                    window=True)
+    daft2 = xrft.dft(da2,
+                    dim=None, shift=True, remove_mean=True,
+                    window=True)
+    npt.assert_almost_equal(cs.values, np.real(daft*np.conj(daft2)))
+
+    npt.assert_almost_equal(np.ma.masked_invalid(cs).mask.sum(), 0.)
+
+def test_parseval():
+    """Test whether the Parseval's relation is satisfied."""
+
+    N = 16
+    da = xr.DataArray(np.random.rand(N,N),
+                    dims=['x','y'], coords={'x':range(N), 'y':range(N)})
+    da2 = xr.DataArray(np.random.rand(N,N),
+                    dims=['x','y'], coords={'x':range(N), 'y':range(N)})
+
+    dim = da.dims
+    delta_x = []
+    for d in dim:
+        coord = da[d]
+        diff = np.diff(coord)
+        # if pd.core.common.is_timedelta64_dtype(diff):
+        #     # convert to seconds so we get hertz
+        #     diff = diff.astype('timedelta64[s]').astype('f8')
+        delta = diff[0]
+        delta_x.append(delta)
+
+    window = np.hanning(N) * np.hanning(N)[:, np.newaxis]
+    ps = xrft.power_spectrum(da, window=True)
+    da_prime = da.values - da.mean(dim=dim).values
+    npt.assert_almost_equal(ps.values.sum(),
+                            (np.asarray(delta_x).prod()
+                            * ((da_prime*window)**2).sum()
+                            ), decimal=5
+                            )
+
+    cs = xrft.cross_spectrum(da, da2, window=True)
+    da2_prime = da2.values - da2.mean(dim=dim).values
+    npt.assert_almost_equal(cs.values.sum(),
+                            (np.asarray(delta_x).prod()
+                            * ((da_prime*window)
+                            * (da2_prime*window)).sum()
+                            ), decimal=5
+                            )
+
+def synthetic_field(N, dL, amp, s):
+    """
+    Generate a synthetic series of size N by N
+    with a spectral slope of s.
+    """
+
+    k = np.fft.fftshift(np.fft.fftfreq(N, dL))
+    l = np.fft.fftshift(np.fft.fftfreq(N, dL))
+    kk, ll = np.meshgrid(k, l)
+    K = np.sqrt(kk**2+ll**2)
+
+    ########
+    # amplitude
+    ########
+    r_kl = np.ma.masked_invalid(np.sqrt(amp*.5*(np.pi)**(-1)
+                                *K**(s-1.))).filled(0.)
+    #r = np.ma.masked_invalid(np.abs(k)**(-slope/2.)).filled(0.)
+    ########
+    # phase
+    ########
+    phi = np.zeros((N, N))
+
+    N_2 = int(N/2)
+    phi_upper_right = 2.*np.pi*np.random.random((N_2-1,
+                                                 N_2-1)) - np.pi
+    phi[N_2+1:,N_2+1:] = phi_upper_right.copy()
+    phi[1:N_2, 1:N_2] = -phi_upper_right[::-1, ::-1].copy()
+
+
+    phi_upper_left = 2.*np.pi*np.random.random((N_2-1,
+                                                N_2-1)) - np.pi
+    phi[N_2+1:,1:N_2] = phi_upper_left.copy()
+    phi[1:N_2, N_2+1:] = -phi_upper_left[::-1, ::-1].copy()
+
+
+    phi_upper_middle = 2.*np.pi*np.random.random(N_2) - np.pi
+    phi[N_2:, N_2] = phi_upper_middle.copy()
+    phi[1:N_2, N_2] = -phi_upper_middle[1:][::-1].copy()
+
+
+    phi_right_middle = 2.*np.pi*np.random.random(N_2-1) - np.pi
+    phi[N_2, N_2+1:] = phi_right_middle.copy()
+    phi[N_2, 1:N_2] = -phi_right_middle[::-1].copy()
+
+
+    phi_edge_upperleft = 2.*np.pi*np.random.random(N_2) - np.pi
+    phi[N_2:, 0] = phi_edge_upperleft.copy()
+    phi[1:N_2, 0] = -phi_edge_upperleft[1:][::-1].copy()
+
+
+    phi_bot_right = 2.*np.pi*np.random.random(N_2) - np.pi
+    phi[0, N_2:] = phi_bot_right.copy()
+    phi[0, 1:N_2] = -phi_bot_right[1:][::-1].copy()
+
+
+    phi_corner_leftbot = 2.*np.pi*np.random.random() - np.pi
+
+
+#     print(phi[N/2-1,N-1], phi[N/2+1,1])
+#     print(phi[N/2+1,N/2+1], phi[N/2-1,N/2-1])
+
+
+#     phi[:N/2, :] = -np.rot90(np.rot90(phi[N/2:, :]))
+#     phi[:N/2, :] = -phi[N/2:, :][::-1,::-1]
+#     i, j = 25, 40
+#     print(phi[N/2+j,N/2+i], -phi[N/2-j,N/2-i])
+
+    for i in range(1, N_2):
+        for j in range(1, N_2):
+            assert (phi[N_2+j, N_2+i] == -phi[N_2-j, N_2-i])
+
+    for i in range(1, N_2):
+        for j in range(1, N_2):
+            assert (phi[N_2+j, N_2-i] == -phi[N_2-j, N_2+i])
+
+    for i in range(1, N_2):
+        assert (phi[N_2, N-i] == -phi[N_2, i])
+        assert (phi[N-i, N_2] == -phi[i, N_2])
+        assert (phi[N-i, 0] == -phi[i, 0])
+        assert (phi[0, i] == -phi[0, N-i])
+    #########
+    # complex fourier amplitudes
+    #########
+    #a = r + 1j*th
+    F_theta = r_kl * np.exp(1j * phi)
+
+    # check that symmetry of FT is satisfied
+    #np.testing.assert_almost_equal(a[1:N/2], a[-1:-N/2:-1].conj())
+
+    theta = np.fft.ifft2(np.fft.ifftshift(F_theta))
+    return np.real(theta)
+
+def test_isotropic_ps_slope(N=512, dL=1., amp=1e1, s=-3.):
+    """Test the spectral slope of isotropic power spectrum."""
+
+    theta = xr.DataArray(synthetic_field(N, dL, amp, s),
+                        dims=['x', 'y'],
+                        coords={'x':range(N), 'y':range(N)})
+    iso_ps = xrft.isotropic_powerspectrum(theta, remove_mean=True,
+                                        density=True)
+    npt.assert_almost_equal(np.ma.masked_invalid(iso_ps[1:]).mask.sum(), 0.)
+    y_fit, a, b = xrft.fit_loglog(iso_ps.freq_r.values[4:],
+                                iso_ps.values[4:])
+
+    npt.assert_allclose(a, s, atol=.1)
+
+def test_isotropic_cs():
+    """Test isotropic cross spectrum"""
+    N = 16
+    da = xr.DataArray(np.random.rand(N,N),
+                    dims=['x','y'], coords={'x':range(N), 'y':range(N)})
+    da2 = xr.DataArray(np.random.rand(N,N),
+                    dims=['x','y'], coords={'x':range(N), 'y':range(N)})
+
+    dim = da.dims
+    delta_x = []
+    for d in dim:
+        coord = da[d]
+        diff = np.diff(coord)
+        # if pd.core.common.is_timedelta64_dtype(diff):
+        #     # convert to seconds so we get hertz
+        #     diff = diff.astype('timedelta64[s]').astype('f8')
+        delta = diff[0]
+        delta_x.append(delta)
+
+    iso_cs = xrft.isotropic_crossspectrum(da, da2, window=True, nbins=int(N/4))
+    npt.assert_almost_equal(np.ma.masked_invalid(iso_cs[1:]).mask.sum(), 0.)
+
+    da2 = xr.DataArray(np.random.rand(N,N),
+                    dims=['lon','lat'], coords={'lon':range(N), 'lat':range(N)})
+    with pytest.raises(ValueError):
+        xrft.isotropic_crossspectrum(da, da2)
--- xrft/xrft.py
+++ xrft/xrft.py
@@ -1,15 +1,51 @@
 import numpy as np
 import xarray as xr
 import pandas as pd
+import functools as ft
 import dask.array as dsar
 
-__all__ = ["dft"]
+__all__ = ["dft","power_spectrum","cross_spectrum",
+            "isotropic_powerspectrum","isotropic_crossspectrum",
+            "fit_loglog"]
 
 
-def dft(da, dim=None, shift=True, remove_mean=True, density=False):
+def _hanning(da, N):
+    """Apply Hanning window"""
+
+    window = np.hanning(N[-1]) * np.hanning(N[-2])[:, np.newaxis]
+
+    # dim = da.dims
+    # coord = da.coords
+
+    # if len(dim) == 3:
+    #     N1 = da.shape[0]
+    #     if da[0].shape != window.shape:
+    #         raise ValueError('The spatial dimensions do not match up')
+    #     for i in range(N1):
+    #         da[i] *= window
+    # elif len(dim) == 4:
+    #     N1, N2 = da.shape[:2]
+    #     if da[0,0].shape != window.shape:
+    #         raise ValueError('The spatial dimensions do not match up')
+    #     for j in range(N1):
+    #         for i in range(N2):
+    #             da[j,i] *= window
+    # elif len(dim) == 2:
+    #     da *= window
+    # else:
+    #     raise ValueError('Data has too many dimensions')
+    da *= window
+
+    return da
+
+def dft(da, dim=None, shift=True, remove_mean=True, window=False):
     """
     Perform discrete Fourier transform of xarray data-array `da` along the
     specified dimensions.
+
+    .. math::
+
+     daft = \mathbb{F}(da - \overline{da})
 
     Parameters
     ----------
@@ -23,14 +59,14 @@ def dft(da, dim=None, shift=True, remove_mean=True, density=False):
     remove_mean : bool (optional)
         If `True`, the mean across the transform dimensions will be subtracted
         before calculating the Fourier transform.
-    density : bool (optional)
-        If `True`, the output will be normalized to give spectral density.
 
     Returns
     -------
     daft : `xarray.DataArray`
         The output of the Fourier transformation, with appropriate dimensions.
     """
+    if np.isnan(da.values).any():
+        raise ValueError("Data cannot take Nans")
 
     if dim is None:
         dim = da.dims
@@ -54,10 +90,13 @@ def dft(da, dim=None, shift=True, remove_mean=True, density=False):
                              "coodinate %s is not evenly spaced" % d)
         delta_x.append(delta)
     # calculate frequencies from coordinates
-    k = [ np.fft.fftfreq(Nx, dx) for (Nx, dx) in zip(N, delta_x)]
+    k = [ np.fft.fftfreq(Nx, dx) for (Nx, dx) in zip(N, delta_x) ]
 
     if remove_mean:
         da = da - da.mean(dim=dim)
+
+    if window:
+        da = _hanning(da, N)
 
     # the hard work
     #f = np.fft.fftn(da.values, axes=axis_num)
@@ -67,23 +106,17 @@ def dft(da, dim=None, shift=True, remove_mean=True, density=False):
     if hasattr(data, 'dask'):
         assert len(axis_num)==1
         f = dsar.fft.fft(data, axis=axis_num[0])
-    else: 
+    else:
         f = np.fft.fftn(data, axes=axis_num)
 
     if shift:
         f = np.fft.fftshift(f, axes=axis_num)
         k = [np.fft.fftshift(l) for l in k]
 
-    dk = [l[1] - l[0] for l in k]
-
-    if density:
-        spectral_volume = reduce(lambda x, y: x*y, dk)
-        f /= spectral_volume
-
     # set up new coordinates for dataarray
     prefix = 'freq_'
     k_names = [prefix + d for d in dim]
-    k_coords = { key: val for (key,val) in zip(k_names, k)}
+    k_coords = {key: val for (key,val) in zip(k_names, k)}
 
     newdims = list(da.dims)
     for anum, d in zip(axis_num, dim):
@@ -96,7 +129,278 @@ def dft(da, dim=None, shift=True, remove_mean=True, density=False):
         else:
             newcoords[d] = k_coords[d]
 
+    dk = [l[1] - l[0] for l in k]
     for this_dk, d in zip(dk, dim):
         newcoords[prefix + d + '_spacing'] = this_dk
 
     return xr.DataArray(f, dims=newdims, coords=newcoords)
+
+def power_spectrum(da, dim=None, shift=True, remove_mean=True, density=True,
+                window=False):
+    """
+    Calculates the power spectrum of da.
+
+    .. math::
+
+     da' = da - \overline{da}
+     ps = \mathbb{F}(da') * {\mathbb{F}(da')}^*
+
+    Parameters
+    ----------
+    da : `xarray.DataArray`
+        The data to be transformed
+    dim : list (optional)
+        The dimensions along which to take the transformation. If `None`, all
+        dimensions will be transformed.
+    shift : bool (optional)
+        Whether to shift the fft output.
+    remove_mean : bool (optional)
+        If `True`, the mean across the transform dimensions will be subtracted
+        before calculating the Fourier transform.
+    density : list (optional)
+        If true, it will normalize the spectrum to spectral density
+
+    Returns
+    -------
+    ps : `xarray.DataArray`
+        Two-dimensional power spectrum
+    """
+
+    if dim is None:
+        dim = da.dims
+
+    # the axes along which to take ffts
+    axis_num = [da.get_axis_num(d) for d in dim]
+
+    N = [da.shape[n] for n in axis_num]
+
+    daft = dft(da,
+            dim=dim, shift=shift, remove_mean=remove_mean,
+            window=window)
+
+    coord = list(daft.coords)
+
+    ps = np.real(daft*np.conj(daft))
+    if density:
+        ps /= (np.asarray(N).prod())**2
+        for i in range(len(dim)):
+            ps /= daft[coord[-i-1]].values
+
+    return xr.DataArray(ps, coords=daft.coords, dims=daft.dims)
+
+def cross_spectrum(da1, da2, dim=None,
+                   shift=True, remove_mean=True, density=True, window=False):
+    """
+    Calculates the cross spectra of da1 and da2.
+
+    .. math::
+
+     da1' = da1 - \overline{da1}; da2' = da2 - \overline{da2}
+     cs = \mathbb{F}(da1') * {\mathbb{F}(da2')}^*
+
+    Parameters
+    ----------
+    da1 : `xarray.DataArray`
+        The data to be transformed
+    da2 : `xarray.DataArray`
+        The data to be transformed
+    dim : list (optional)
+        The dimensions along which to take the transformation. If `None`, all
+        dimensions will be transformed.
+    shift : bool (optional)
+        Whether to shift the fft output.
+    remove_mean : bool (optional)
+        If `True`, the mean across the transform dimensions will be subtracted
+        before calculating the Fourier transform.
+    density : list (optional)
+        If true, it will normalize the spectrum to spectral density
+
+    Returns
+    -------
+    cs : `xarray.DataArray`
+        Two-dimensional cross spectrum
+    """
+
+    if dim is None:
+        dim = da1.dims
+        dim2 = da2.dims
+        if dim != dim2:
+            raise ValueError('The two datasets have different dimensions')
+
+    # the axes along which to take ffts
+    axis_num = [da1.get_axis_num(d) for d in dim]
+
+    N = [da1.shape[n] for n in axis_num]
+
+    daft1 = dft(da1, dim=dim,
+                shift=shift, remove_mean=remove_mean, window=window)
+    daft2 = dft(da2, dim=dim,
+                shift=shift, remove_mean=remove_mean, window=window)
+
+    coord = list(daft1.coords)
+
+    cs = np.real(daft1 * np.conj(daft2))
+    if density:
+        cs /= (np.asarray(N).prod())**2
+        for i in range(int(len(dim))):
+            cs /= daft1[coord[-i-1]].values
+
+    return xr.DataArray(cs, coords=daft1.coords, dims=daft1.dims)
+
+def _azimuthal_avg(k, l, f, nbins=64):
+    """
+    Takes the azimuthal average of a given field.
+    """
+
+    kk, ll = np.meshgrid(k, l)
+    K = np.sqrt(kk**2 + ll**2)
+    if k.max() > l.max():
+        ki = np.linspace(0., l.max(), nbins)
+    else:
+        ki = np.linspace(0., k.max(), nbins)
+
+    kidx = np.digitize(K.ravel(), ki)
+    area = np.bincount(kidx)
+
+    kr = np.ma.masked_invalid(np.bincount(kidx,
+                                          weights=K.ravel()) / area
+                                         )
+    iso_f = np.ma.masked_invalid(np.bincount(kidx,
+                                            weights=f.values.ravel())
+                                            / area
+                                            ) * kr
+    return kr, iso_f
+
+def isotropic_powerspectrum(da, dim=None, shift=True, remove_mean=True,
+                       density=True, window=False, nbins=64):
+    """
+    Calculates the isotropic spectrum from the
+    two-dimensional power spectrum by taking the
+    azimuthal average.
+
+    ..math::
+
+     iso_ps = k_r \frac{1}{N_{\theta}} \sum_{N_{\theta}} |\mathbb{F}(da')|^2
+
+    Parameters
+    ----------
+    da : `xarray.DataArray`
+        The data to be transformed
+    dim : list (optional)
+        The dimensions along which to take the transformation. If `None`, all
+        dimensions will be transformed.
+    shift : bool (optional)
+        Whether to shift the fft output.
+    remove_mean : bool (optional)
+        If `True`, the mean across the transform dimensions will be subtracted
+        before calculating the Fourier transform.
+    density : list (optional)
+        If true, it will normalize the spectrum to spectral density
+
+    Returns
+    -------
+    iso_ps : `xarray.DataArray`
+        Isotropic power spectrum
+    """
+
+    if dim is None:
+        dim = da.dims
+
+    ps = power_spectrum(da, dim=dim, shift=shift,
+                       remove_mean=remove_mean, density=density,
+                       window=window)
+    if len(ps.dims) > 2:
+        raise ValueError('The data set has too many dimensions')
+
+    k = ps[ps.dims[0]].values
+    l = ps[ps.dims[1]].values
+
+    kr, iso_ps = _azimuthal_avg(k, l, ps, nbins=nbins)
+
+    return xr.DataArray(iso_ps, dims=['freq_r'],
+                        coords={'freq_r':kr})
+
+def isotropic_crossspectrum(da1, da2,
+                        dim=None, shift=True, remove_mean=True,
+                        density=True, window=False, nbins=64):
+    """
+    Calculates the isotropic spectrum from the
+    two-dimensional power spectrumby taking the
+    azimuthal average.
+
+    ..math::
+
+     iso_ps = k_r \frac{1}{N_{\theta}} \sum_{N_{\theta}} \\
+            (\mathbb{F}(da1') \times {\mathbb{F}(da2')}^* )
+
+    Parameters
+    ----------
+    da1 : `xarray.DataArray`
+        The data to be transformed
+    da2 : `xarray.DataArray`
+        The data to be transformed
+    dim : list (optional)
+        The dimensions along which to take the transformation. If `None`, all
+        dimensions will be transformed.
+    shift : bool (optional)
+        Whether to shift the fft output.
+    remove_mean : bool (optional)
+        If `True`, the mean across the transform dimensions will be subtracted
+        before calculating the Fourier transform.
+    density : list (optional)
+        If true, it will normalize the spectrum to spectral density
+
+    Returns
+    -------
+    iso_cs : `xarray.DataArray`
+        Isotropic cross spectrum
+    """
+
+    if dim is None:
+        dim = da1.dims
+        dim2 = da2.dims
+        if dim != dim2:
+            raise ValueError('The two datasets have different dimensions')
+
+    cs = cross_spectrum(da1, da2, dim=dim, shift=shift,
+                       remove_mean=remove_mean, density=density,
+                       window=window)
+    if len(cs.dims) > 2:
+        raise ValueError('The data set has too many dimensions')
+
+    k = cs[cs.dims[0]].values
+    l = cs[cs.dims[1]].values
+
+    kr, iso_cs = _azimuthal_avg(k, l, cs, nbins=nbins)
+
+    return xr.DataArray(iso_cs, dims=['freq_r'],
+                        coords={'freq_r':kr})
+
+def fit_loglog(x, y):
+    """
+    Fit a line to isotropic spectra in log-log space
+
+    Parameters
+    ----------
+    x : `numpy.array`
+        Coordinate of the data
+    y : `numpy.array`
+        data
+
+    Returns
+    -------
+    y_fit : `numpy.array`
+        The linear fit
+    a : float64
+        Slope of the fit
+    b : float64
+        Intercept of the fit
+    """
+    # fig log vs log
+    p = np.polyfit(np.log2(x), np.log2(y), 1)
+    y_fit = 2**(np.log2(x)*p[0] + p[1])
+    #A = np.vstack([np.log2(x), np.ones(len(x))]).T
+    #a, b = np.linalg.lstsq(A, np.log2(y))[0]
+    #y_fit = 2**(np.log2(x)*a + b)
+
+    return y_fit, p[0], p[1]
