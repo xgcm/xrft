743449a29d38b762c9236c9d6e51cc6220157380
Takaya Uchida <takaya@ldeo.columbia.edu>
2019-04-01 11:40:42 -0400

    versioneer for init file

--- xrft/__init__.py
+++ xrft/__init__.py
@@ -1,6 +1,5 @@
-from .version import __version__  # noqa
-from .xrft import *  # noqa
-
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+from .xrft import *  # noqa
