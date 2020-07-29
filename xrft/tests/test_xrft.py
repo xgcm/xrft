b6d2a1b2c1cff50788aab44ed8681a7345b96486
Takaya Uchida <takaya@ldeo.columbia.edu>
2020-07-29 14:13:35 +0200

    removed commented out line

--- xrft/tests/test_xrft.py
+++ xrft/tests/test_xrft.py
@@ -177,7 +177,7 @@ class TestDFTImag(object):
                                     coords=[time])
 
         ft = xrft.dft(da, shift=False)
-        # num = cftime.date2num(time,'hours since 1800-01-01 00:00:00','noleap')
+        
         dt = np.diff(time)[0].total_seconds()
         freq_time_expected = np.fft.fftfreq(Nt, dt)
         npt.assert_allclose(ft['freq_time'], freq_time_expected)
