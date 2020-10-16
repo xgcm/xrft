5335123e16faad1b6e1e21c4fd256df03b4941a2
Navid Constantinou <navidcy@gmail.com>
2020-10-17 07:14:41 +1100

    better organizes test_detrend + some comments

--- xrft/xrft.py
+++ xrft/xrft.py
@@ -150,8 +150,9 @@ def detrend_wrap(detrend_func):
 def _apply_detrend(da, axis_num):
     """Wrapper function for applying detrending"""
     if len(axis_num) == 1:
-        func = detrend_wrap(detrendn)
-        da = func(da,axes=axis_num)
+        poly_coefs = da.polyfit(dim=da.dims[axis_num], deg=1)
+        
+        return a - (poly_coefs.data_vars['polyfit_coefficients'][0]*a[a.dims[axes[0]]] + poly_coefs.data_vars['polyfit_coefficients'][1])        
     elif da.chunks:
         func = detrend_wrap(detrendn)
         da = xr.DataArray(func(da.data, axes=axis_num),
