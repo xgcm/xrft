65be84875b8ec7c436c7433101e8777be2c9f494
Ryan Abernathey <ryan.abernathey@gmail.com>
2020-11-05 09:48:57 -0500

    black

--- xrft/detrend.py
+++ xrft/detrend.py
@@ -7,40 +7,50 @@ import xarray as xr
 import scipy.signal as sps
 import scipy.linalg as spl
 
-def detrend(da, dim, detrend_type='constant'):
+
+def detrend(da, dim, detrend_type="constant"):
     """Detrend a DataArray along dimensions described by dim"""
 
-    if detrend_type not in ['constant','linear', None]:
-        raise NotImplementedError("%s is not a valid detrending option. Valid "
-                                  "options are: 'constant','linear', or None."
-                                  % detrend_type)
+    if detrend_type not in ["constant", "linear", None]:
+        raise NotImplementedError(
+            "%s is not a valid detrending option. Valid "
+            "options are: 'constant','linear', or None." % detrend_type
+        )
 
     if detrend_type is None:
         return da
-    elif detrend_type == 'constant':
+    elif detrend_type == "constant":
         return da - da.mean(dim=dim)
-    elif detrend_type == 'linear':
+    elif detrend_type == "linear":
         data = da.data
         axis_num = [da.get_axis_num(d) for d in dim]
-        chunks = getattr(data, 'chunks', None)
+        chunks = getattr(data, "chunks", None)
         if chunks:
             axis_chunks = [data.chunks[a] for a in axis_num]
-            if not all([len(ac)==1 for ac in axis_chunks]):
-                raise ValueError('Contiguous chunks required for detrending.')
+            if not all([len(ac) == 1 for ac in axis_chunks]):
+                raise ValueError("Contiguous chunks required for detrending.")
         if len(dim) == 1:
-            dt = xr.apply_ufunc(sps.detrend, da, axis_num[0],
-                                output_dtypes=[da.dtype],
-                                dask='parallelized')
+            dt = xr.apply_ufunc(
+                sps.detrend,
+                da,
+                axis_num[0],
+                output_dtypes=[da.dtype],
+                dask="parallelized",
+            )
         elif len(dim) == 2:
-            dt = xr.apply_ufunc(_detrend_2d_ufunc,
-                    da,
-                    input_core_dims=[dim],
-                    output_core_dims=[dim],
-                    output_dtypes=[da.dtype],
-                    vectorize=True,
-                    dask='parallelized')
-        else: # pragma: no cover
-            raise NotImplementedError("Only 1D and 2D detrending are implemented so far.")
+            dt = xr.apply_ufunc(
+                _detrend_2d_ufunc,
+                da,
+                input_core_dims=[dim],
+                output_core_dims=[dim],
+                output_dtypes=[da.dtype],
+                vectorize=True,
+                dask="parallelized",
+            )
+        else:  # pragma: no cover
+            raise NotImplementedError(
+                "Only 1D and 2D detrending are implemented so far."
+            )
 
     return dt
 
--- xrft/tests/test_detrend.py
+++ xrft/tests/test_detrend.py
@@ -9,8 +9,10 @@ import xarray.testing as xrt
 import xrft
 from xrft.detrend import detrend
 
+
 def detrended_noise(N, amplitude=1.0):
-    return sps.detrend(amplitude*np.random.rand(N))
+    return sps.detrend(amplitude * np.random.rand(N))
+
 
 def noise(dims, shape):
     assert len(dims) == len(shape)
@@ -21,32 +23,41 @@ def noise(dims, shape):
     da = xr.DataArray(data, dims=dims, coords=coords)
     return da
 
+
 @pytest.mark.parametrize(
-    'array_dims, array_shape, detrend_dim, chunks, linear_error',
+    "array_dims, array_shape, detrend_dim, chunks, linear_error",
     (
-        (['x'], [16], 'x', None, None),
-        (['y', 'x'], [32, 16], 'x', None, None),
-        (['y', 'x'], [32, 16], 'x', {'y': 4}, None),
-        (['y', 'x'], [32, 16], 'y', None, None),
-        (['y', 'x'], [32, 16], 'y', {'x': 4}, None),
-        (['time', 'y', 'x'], [4, 32, 16], 'x', None, None),
-        (['time', 'y', 'x'], [4, 32, 16], 'x', {'y': 4}, None),
-        (['time', 'y', 'x'], [4, 32, 16], 'x', {'time': 1, 'y': 4}, None),
+        (["x"], [16], "x", None, None),
+        (["y", "x"], [32, 16], "x", None, None),
+        (["y", "x"], [32, 16], "x", {"y": 4}, None),
+        (["y", "x"], [32, 16], "y", None, None),
+        (["y", "x"], [32, 16], "y", {"x": 4}, None),
+        (["time", "y", "x"], [4, 32, 16], "x", None, None),
+        (["time", "y", "x"], [4, 32, 16], "x", {"y": 4}, None),
+        (["time", "y", "x"], [4, 32, 16], "x", {"time": 1, "y": 4}, None),
         # error cases for linear detrending
-        (['x'], [16], 'x', {'x': 1}, ValueError),
-        (['y', 'x'], [32, 16], 'x', {'x': 4}, ValueError),
-    )
+        (["x"], [16], "x", {"x": 1}, ValueError),
+        (["y", "x"], [32, 16], "x", {"x": 4}, ValueError),
+    ),
 )
-@pytest.mark.parametrize('detrend_type', [None, 'constant', 'linear'])
-@pytest.mark.parametrize('trend_amplitude', [0.01, 100])
-def test_detrend_1D(array_dims, array_shape, detrend_dim, chunks, detrend_type, trend_amplitude, linear_error):
+@pytest.mark.parametrize("detrend_type", [None, "constant", "linear"])
+@pytest.mark.parametrize("trend_amplitude", [0.01, 100])
+def test_detrend_1D(
+    array_dims,
+    array_shape,
+    detrend_dim,
+    chunks,
+    detrend_type,
+    trend_amplitude,
+    linear_error,
+):
     da_original = noise(array_dims, array_shape)
     da_trend = da_original + trend_amplitude * da_original[detrend_dim]
     if chunks:
         da_trend = da_trend.chunk(chunks)
 
     # bail out if we are expecting an error
-    if detrend_type=='linear' and linear_error:
+    if detrend_type == "linear" and linear_error:
         with pytest.raises(linear_error):
             detrend(da_trend, detrend_dim, detrend_type=detrend_type)
         return
@@ -55,35 +66,42 @@ def test_detrend_1D(array_dims, array_shape, detrend_dim, chunks, detrend_type, 
     assert detrended.chunks == da_trend.chunks
     if detrend_type is None:
         xrt.assert_equal(detrended, da_trend)
-    elif detrend_type == 'constant':
+    elif detrend_type == "constant":
         xrt.assert_allclose(detrended, da_trend - da_trend.mean(dim=detrend_dim))
-    elif detrend_type == 'linear':
+    elif detrend_type == "linear":
         xrt.assert_allclose(detrended, da_original)
 
 
 # always detrend on x y dims
 @pytest.mark.parametrize(
-    'array_dims, array_shape, chunks',
+    "array_dims, array_shape, chunks",
     (
-        (['y', 'x'], [32, 16], None),
-        (['z', 'y', 'x'], [2, 32, 16], None),
-        (['z', 'y', 'x'], [2, 32, 16], {'z': 1}),
-    )
+        (["y", "x"], [32, 16], None),
+        (["z", "y", "x"], [2, 32, 16], None),
+        (["z", "y", "x"], [2, 32, 16], {"z": 1}),
+    ),
 )
-@pytest.mark.parametrize('detrend_type', [None, 'constant', 'linear'])
-@pytest.mark.parametrize('trend_amplitude', [{'x': 0.1, 'y': 0.1}, {'x': 10., 'y': 0.01}])
+@pytest.mark.parametrize("detrend_type", [None, "constant", "linear"])
+@pytest.mark.parametrize(
+    "trend_amplitude", [{"x": 0.1, "y": 0.1}, {"x": 10.0, "y": 0.01}]
+)
 def test_detrend_2D(array_dims, array_shape, chunks, detrend_type, trend_amplitude):
     da_original = noise(array_dims, array_shape)
-    da_trend = da_original + trend_amplitude['x'] * da_original['x'] + trend_amplitude['y'] + da_original['x']
+    da_trend = (
+        da_original
+        + trend_amplitude["x"] * da_original["x"]
+        + trend_amplitude["y"]
+        + da_original["x"]
+    )
     if chunks:
         da_trend = da_trend.chunk(chunks)
 
-    detrend_dim = ['y', 'x']
+    detrend_dim = ["y", "x"]
     detrended = detrend(da_trend, detrend_dim, detrend_type=detrend_type)
     assert detrended.chunks == da_trend.chunks
     if detrend_type is None:
         xrt.assert_equal(detrended, da_trend)
-    elif detrend_type == 'constant':
+    elif detrend_type == "constant":
         xrt.assert_allclose(detrended, da_trend - da_trend.mean(dim=detrend_dim))
-    elif detrend_type == 'linear':
+    elif detrend_type == "linear":
         xrt.assert_allclose(detrended, da_original)
