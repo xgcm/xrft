d6cbc0ab383ab2c6406b2145601eb82c016b448f
Takaya Uchida <takaya@ldeo.columbia.edu>
2020-11-25 13:57:10 +0100

    parallelize isotropize

--- xrft/xrft.py
+++ xrft/xrft.py
@@ -5,6 +5,7 @@ import functools as ft
 from functools import reduce
 
 import numpy as np
+import numpy_groupies
 import xarray as xr
 import pandas as pd
 
@@ -647,29 +648,87 @@ def cross_phase(
     return cp
 
 
-def _radial_wvnum(k, l, N, nfactor):
-    """Creates a radial wavenumber based on two horizontal wavenumbers
-    along with the appropriate index map
-    """
+def _binned_agg(
+    array: np.ndarray,
+    indices: np.ndarray,
+    num_bins: int,
+    *,
+    func,
+    fill_value,
+    dtype,
+) -> np.ndarray:
+    """NumPy helper function for aggregating over bins."""
+    mask = np.logical_not(np.isnan(indices))
+    int_indices = indices[mask].astype(int)
+    shape = array.shape[:-indices.ndim] + (num_bins,)
+    result = numpy_groupies.aggregate(
+        int_indices, array[..., mask],
+        func=func,
+        size=num_bins,
+        fill_value=fill_value,
+        dtype=dtype,
+        axis=-1,
+    )
+    return result
 
-    # compute target wavenumbers
-    k = k.values
-    l = l.values
-    K = np.sqrt(k[np.newaxis, :] ** 2 + l[:, np.newaxis] ** 2)
-    nbins = int(N / nfactor)
-    if k.max() > l.max():
-        ki = np.linspace(0.0, l.max(), nbins)
-    else:
-        ki = np.linspace(0.0, k.max(), nbins)
+def _groupby_bins_agg(
+    array: xr.DataArray,
+    group: xr.DataArray,
+    bins,
+    func='sum',
+    fill_value=0,
+    dtype=None,
+    **cut_kwargs,
+) -> xr.DataArray:
+    """Faster equivalent of Xarray's groupby_bins(...).sum()."""
+    # TODO: implement this upstream in xarray:
+    # https://github.com/pydata/xarray/issues/4473
+    binned = pd.cut(np.ravel(group), bins, **cut_kwargs)
+    new_dim_name = group.name + "_bins"
+    indices = group.copy(data=binned.codes.reshape(group.shape))
 
-    # compute bin index
-    kidx = np.digitize(np.ravel(K), ki)
-    # compute number of points for each wavenumber
-    area = np.bincount(kidx)
-    # compute the average radial wavenumber for each bin
-    kr = np.bincount(kidx, weights=K.ravel()) / np.ma.masked_where(area == 0, area)
+    result = xr.apply_ufunc(
+        _binned_agg, array, indices,
+        input_core_dims=[indices.dims, indices.dims],
+        output_core_dims=[[new_dim_name]],
+        output_dtypes=[array.dtype],
+        dask_gufunc_kwargs=dict(
+            output_sizes={new_dim_name: binned.categories.size},
+        ),
+        kwargs={
+            'num_bins': binned.categories.size,
+            'func': func,
+            'fill_value': fill_value,
+            'dtype': dtype,
+        },
+        dask='parallelized',
+    )
+    result.coords[new_dim_name] = binned.categories
+    return result
 
-    return ki, kr[1:-1]
+# def _radial_wvnum(k, l, N, nfactor):
+#     """Creates a radial wavenumber based on two horizontal wavenumbers
+#     along with the appropriate index map
+#     """
+#
+#     # compute target wavenumbers
+#     k = k.values
+#     l = l.values
+#     K = np.sqrt(k[np.newaxis, :] ** 2 + l[:, np.newaxis] ** 2)
+#     nbins = int(N / nfactor)
+#     if k.max() > l.max():
+#         ki = np.linspace(0.0, l.max(), nbins)
+#     else:
+#         ki = np.linspace(0.0, k.max(), nbins)
+#
+#     # compute bin index
+#     kidx = np.digitize(np.ravel(K), ki)
+#     # compute number of points for each wavenumber
+#     area = np.bincount(kidx)
+#     # compute the average radial wavenumber for each bin
+#     kr = np.bincount(kidx, weights=K.ravel()) / np.ma.masked_where(area == 0, area)
+#
+#     return ki, kr[1:-1]
 
 
 def isotropize(ps, fftdim, nfactor=4):
@@ -697,15 +756,32 @@ def isotropize(ps, fftdim, nfactor=4):
     k = ps[fftdim[1]]
     l = ps[fftdim[0]]
     N = [k.size, l.size]
-    ki, kr = _radial_wvnum(k, l, min(N), nfactor)
+    nbins = int(min(N) / nfactor)
+    # ki, kr = _radial_wvnum(k, l, min(N), nfactor)
+    freq_r = np.sqrt(k**2 + l**2).rename('freq_r')
+    kr = _groupby_bins_agg(
+        freq_r,
+        freq_r,
+        bins=nbins,
+        func='mean')
 
     # average azimuthally
-    ps = ps.assign_coords(freq_r=np.sqrt(k ** 2 + l ** 2))
+    # ps = ps.assign_coords(freq_r=np.sqrt(k ** 2 + l ** 2))
+    # iso_ps = (
+    #     ps.groupby_bins("freq_r", bins=ki, labels=kr)
+    #     .mean()
+    #     .rename({"freq_r_bins": "freq_r"})
+    # )
     iso_ps = (
-        ps.groupby_bins("freq_r", bins=ki, labels=kr)
-        .mean()
+        _groupby_bins_agg(
+        ps,
+        freq_r,
+        bins=nbins,
+        func='mean')
         .rename({"freq_r_bins": "freq_r"})
+        .drop_vars('freq_r')
     )
+    iso_ps.coords['freq_r'] = kr.data
     return iso_ps * iso_ps.freq_r
 
 
