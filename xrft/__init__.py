8a959f693789f879ae7e83b28930bb8f2f659089
Takaya Uchida <takaya@ldeo.columbia.edu>
2021-09-01 16:51:15 +0200

    pkg_resources

--- xrft/__init__.py
+++ xrft/__init__.py
@@ -1,2 +1,12 @@
-from .xrft import *  # noqa
-from .detrend import detrend
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:  # noqa: F401
+    # package is not installed
+    pass
+
+del get_distribution, DistributionNotFound
+
+# from .xrft import *  # noqa
+# from .detrend import detrend
