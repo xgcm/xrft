1bbb18a21cea846d7262e8568139191d8ce980c2
Takaya Uchida <takaya@ldeo.columbia.edu>
2021-09-01 15:26:42 +0200

    black (sigh)

--- xrft/__init__.py
+++ xrft/__init__.py
@@ -1,7 +1,7 @@
-from ._version import get_versions 
-  
-__version__ = get_versions()["version"] 
-del get_versions 
- 
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
+
 from .xrft import *  # noqa
 from .detrend import detrend
